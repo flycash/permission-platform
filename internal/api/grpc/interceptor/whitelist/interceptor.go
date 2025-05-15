1ac812398ec7db12dde607c7ffe7959990f8e2b3
Deng Ming <ming_flycash@qq.com>
2025-05-15 12:21:07 +0800

    white list 重构

--- internal/api/grpc/interceptor/whitelist/interceptor.go
+++ internal/api/grpc/interceptor/whitelist/interceptor.go
@@ -8,26 +8,40 @@ import (
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	"strings"
+	"sync"
 )
 
+// InterceptorBuilder 它必须放在 JWT 之后
 type InterceptorBuilder struct {
 	// Biz ID  的白名单
 	WhiteList []int64
+	mutex     *sync.RWMutex
+}
+
+func NewInterceptorBuilder(whiteList []int64) *InterceptorBuilder {
+	return &InterceptorBuilder{WhiteList: whiteList, mutex: &sync.RWMutex{}}
 }
 
 func (i *InterceptorBuilder) Build() grpc.UnaryServerInterceptor {
 	return func(ctx context.Context, req any, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp any, err error) {
-		// 操作 BusinessConfig 的代码
+		i.mutex.RLock()
+		defer i.mutex.RUnlock()
 		if strings.Contains(info.FullMethod, "BusinessConfig") {
 			bizID, err := jwt.GetBizIDFromContext(ctx)
 			if err != nil {
-				// 没有 Biz ID 肯定不能操作
-				return nil, err
+				return nil, status.Errorf(codes.PermissionDenied, "没有权限，原因: %s", err.Error())
 			}
 			if !slice.Contains(i.WhiteList, bizID) {
-				return nil, status.Errorf(codes.PermissionDenied, "没有权限")
+				return nil, status.Errorf(codes.PermissionDenied, "不在白名单内")
 			}
 		}
 		return handler(ctx, req)
 	}
+}
+
+func (i *InterceptorBuilder) UpdateWhiteList(list []int64) {
+	// 并发安全
+	i.mutex.Lock()
+	defer i.mutex.Unlock()
+	i.WhiteList = list
 }
