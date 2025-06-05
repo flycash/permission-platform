016f6e92d2137e7078f264fb5161b1730352fd85
Deng Ming <ming_flycash@qq.com>
2025-06-05 17:32:58 +0800

    删除 Redis Cache 中更新 Redis 的操作

--- pkg/permission/internal/redis_cached_client.go
+++ pkg/permission/internal/redis_cached_client.go
@@ -3,10 +3,7 @@ package internal
 import (
 	"context"
 	"encoding/json"
-	"time"
-
 	permissionv1 "gitee.com/flycash/permission-platform/api/proto/gen/permission/v1"
-	"github.com/ecodeclub/ekit/slice"
 	"github.com/redis/go-redis/v9"
 	"google.golang.org/grpc"
 )
@@ -19,16 +16,13 @@ type RedisCachedClient struct {
 	client permissionv1.PermissionServiceClient
 	// redis 缓存
 	rd redis.Cmdable
-	// 过期时间
-	expiration time.Duration
 }
 
-func NewRedisCachedClient(client permissionv1.PermissionServiceClient, rd redis.Cmdable, expiration time.Duration) *RedisCachedClient {
+func NewRedisCachedClient(client permissionv1.PermissionServiceClient, rd redis.Cmdable) *RedisCachedClient {
 	return &RedisCachedClient{
 		baseCachedClient: baseCachedClient{},
 		client:           client,
 		rd:               rd,
-		expiration:       expiration,
 	}
 }
 
@@ -37,7 +31,7 @@ func (c *RedisCachedClient) Name() string {
 }
 
 func (c *RedisCachedClient) CheckPermission(ctx context.Context, in *permissionv1.CheckPermissionRequest, opts ...grpc.CallOption) (*permissionv1.CheckPermissionResponse, error) {
-	// 1. 从redis缓存取
+	// 1. 从redis缓存取，注意你不需要更新 Redis 的缓存
 	userPermission, err := c.getFromCache(ctx, in.GetPermission().GetBizId(), in.GetUid())
 	if err == nil {
 		resp, err1 := c.checkPermission(userPermission, in)
@@ -46,14 +40,7 @@ func (c *RedisCachedClient) CheckPermission(ctx context.Context, in *permissionv
 		}
 	}
 	// 2. 从 client 取
-	resp, err := c.client.CheckPermission(ctx, in, opts...)
-	if err != nil {
-		return nil, err
-	}
-	if resp.Allowed {
-		c.setToCache(ctx, in)
-	}
-	return resp, nil
+	return c.client.CheckPermission(ctx, in, opts...)
 }
 
 func (c *RedisCachedClient) getFromCache(ctx context.Context, bizID, userID int64) (UserPermission, error) {
@@ -67,24 +54,4 @@ func (c *RedisCachedClient) getFromCache(ctx context.Context, bizID, userID int6
 		return UserPermission{}, err
 	}
 	return userPermission, nil
-}
-
-func (c *RedisCachedClient) setToCache(ctx context.Context, in *permissionv1.CheckPermissionRequest) {
-	perm := in.GetPermission()
-	up := UserPermission{
-		UserID: in.GetUid(),
-		BizID:  perm.GetBizId(),
-		Permissions: slice.Map(perm.GetActions(), func(_ int, src string) PermissionV1 {
-			return PermissionV1{
-				Resource: Resource{
-					Key:  perm.GetResourceKey(),
-					Type: perm.GetResourceType(),
-				},
-				Action: src,
-				Effect: "allow",
-			}
-		}),
-	}
-	data, _ := json.Marshal(up)
-	c.rd.Set(ctx, c.cacheKey(perm.GetBizId(), in.GetUid()), string(data), c.expiration)
 }
